a976a61ca00f36837fc87dcc3fe18e590bb596a3
marcos rocha <mrocha@cos.ufrj.br>
2016-10-17 22:41:38 -0200

    programa n termina. Grafo aparentemente esta como na versao python envio dos operandos tb, so q no Java trava no segundo
    Python
    workerid: 0
    dstid: 1
    dstport: 0
    value[srcport]: [0, 1]
    workerid: 0
    dstid: 9
    dstport: 0
    value[srcport]: [0, 1]
    
    Java
    workerid:0
    dstid:1
    dstport:0
    value[srcport]:[I@4f91603c
    workerid:0
    dstid:9
    dstport:0
    value[srcport]:[I@5ecdcc14

--- src/LCS.java
+++ src/LCS.java
@@ -8,6 +8,14 @@ import java.nio.file.Paths;
  */
 public class LCS {
 
+    static int block;
+    static int gW;
+    static int gH;
+    static int sizeA;
+    static int sizeB;
+    static String sA;
+    static String sB;
+
     public static void main(String[] args) {
 
         NodeFunction printLCS = (NodeFunction & Serializable) (Object[] inputs) -> {
@@ -18,28 +26,20 @@ public class LCS {
 
         NodeFunction compute = (NodeFunction & Serializable) (Object[] inputs) -> {
 
-            System.out.println("Score: " + ((Object[])(inputs[0]))[-1]);
-            LCS(i, j, args);
-            return null;
+            //System.out.println("Score: " + ((Object[])(inputs[0]))[-1]);
+            int i = (int)inputs[inputs.length - 2];
+            int j = (int)inputs[inputs.length - 1];
+            LCS(i, j, inputs);
+            return LCS(i, j, inputs);
         };
 
-
-        int nprocs = 1;
+        int nprocs = 1;//int(sys.argv[3])
         DFGraph lcsGraph = new DFGraph();
         Scheduler sched = new Scheduler(lcsGraph, nprocs, false);
 
-        String nameA = "testA1k.txt";//sys.argv[1]
-        String nameB = "testB1k.txt";//sys.argv[2]
-        /*BufferedReader fA = null;
-        BufferedReader fB = null;
-        try {
-            fA = new BufferedReader(new InputStreamReader(new FileInputStream(nameA)));
-            fB = new BufferedReader(new InputStreamReader(new FileInputStream(nameB)));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }*/
-        String sA = "";
-        String sB = "";
+        String nameA = "seqA.txt";//sys.argv[1]
+        String nameB = "seqB.txt";//sys.argv[2]
+        String graph = "";
         try {
         sA = new String(Files.readAllBytes(Paths.get(nameA)));
         sB = new String(Files.readAllBytes(Paths.get(nameB)));
@@ -49,18 +49,16 @@ public class LCS {
             e.printStackTrace();
         }
 
-
-        int sizeA = sA.length() - sA.charAt(-1) == '\n'?1:0;
-        int sizeB = sB.length() - sB.charAt(-1) == '\n'?1:0;
+        sizeA = sA.length() - (sA.charAt(sA.length()-1) == '\n' ? 1 : 0);
+        sizeB = sB.length() - (sB.charAt(sB.length()-1) == '\n' ? 1 : 0);
         System.out.println("Sizes" +"("+sizeB+","+sizeA+")");
-        nprocs = 1;//int(sys.argv[3])
-        int block = 1;//int(sys.argv[4])
-        int gH = 0;//int(math.ceil(float(sizeB)/block));
-        int gW = 0;//int(math.ceil(float(sizeA)/block));
+
+        block = 1;//int(sys.argv[4])
+        gH = (int)(Math.ceil((float)(sizeB)/block));
+        gW = (int)(Math.ceil((float)(sizeA)/block));
         System.out.println("Grid "+gH+" x "+gW);
 
-
-        Object[][]G = new Object[gH][gW]; //[[N2D(compute,inputs(i,j),i,j) for j in xrange(gW)] for i in xrange(gH)]
+        Object[][]G = new Object[gH][gW];
         for(int i=0; i< gH;i++){
             for(int j=0; j< gW; j++ ){
                 G[i][j] = new N2D(compute, inputs(i, j), i, j);
@@ -80,18 +78,23 @@ public class LCS {
                 if(i > 0){
                     //create edge from  upper neighbor
                     //#print 'Edge (%d,%d) -> (%d,%d)[%d]' % (i-1,j,i,j,0)
-                    ((N2D)G[i-1][j]).add_edge(((Node)G[i][j]),0,0);
+                    ((N2D)G[i-1][j]).add_edge(((Node)G[i][j]), 0, 0);
+                    graph+= "N2D_"+(i-1)+"_"+j+ "-> Node_"+i+"_"+j+ "_" + 0 + "_"+0+";\n";
+
                 }if(j > 0){
                     //create edge from left neighor
                     //#print 'Edge (%d,%d) -> (%d,%d)[%d]' % (i,j-1,i,j, int(i>0))
-                    ((N2D)G[i][j-1]).add_edge(G[i][j],int(i>0),1);
+                    ((N2D)G[i][j-1]).add_edge((Node)G[i][j], (i > 0 ? 1 : 0) ,1);
+                    graph+= "N2D_"+(i)+"_"+(j-1)+ "-> Node_"+i+"_"+j+ "_" + (i > 0 ? 1 : 0) + "_"+0+";\n";
                 }
             }
         }
 
         Node R = new Node(printLCS, 1);
         lcsGraph.add(R);
-        ((N2D)G[gH-1][gW-1]).add_edge(R,0);
+        ((N2D)G[gH-1][gH-1]).add_edge(R, 0, 0);
+        graph+= "N2D_"+(gH-1)+"_"+(gH-1)+ "-> R_"+ 0 + "_"+0+";\n";
+        System.out.println("Graph\n" +graph);
         sched.start();
 
 
@@ -104,4 +107,83 @@ public class LCS {
             return 1;
         return 2;
     }
+
+
+    static Object[] LCS(int i, int j, Object[] oper){
+        int startA = j*block;
+        int endA = 0;
+        if ((j+1) == gW)
+            endA = sizeA;
+        else
+            endA = startA+block;
+
+        int startB = i*block;
+        int endB = 0;
+        if ((i+1) == gH)
+            endB = sizeB;
+        else
+            endB = startB+block;
+        int lsizeA = endA - startA;
+        int lsizeB = endB - startB;
+    //print 'Node (%d,%d) calculates (%d,%d) - (%d,%d)' % (i,j, startB,startA,endB,endA)
+
+        int SM[][] = new int[lsizeA+1][lsizeB+1];
+        for(int x = 0; x < lsizeA+1; x++){
+            for(int y = 0; y < lsizeB+1; y++) {
+                SM[x][y] = 0;
+            }
+        }
+
+        int port = 0;
+        if(i>0) {
+            for (int x = 0; x < ((Object[])oper[0]).length; x++) {
+                SM[0][x] = (int)((Object[])oper[0])[x];
+
+            }
+            port = port + 1;
+        }
+        if(j>0) {
+            for(int x=0; x < ((Object[])oper[port]).length; x++) {
+                SM[x][0] = (int)((Object[])oper[port])[x];
+            }
+        }
+
+        //print 'Antes: Matrix (%d, %d)' %(i,j)
+        //printMatrix(SM)
+        for(int ii = 1; ii <lsizeB+1; ii++){
+            for(int jj = 1; jj < lsizeA+1; jj++) {
+                if (sB.charAt(startB + ii - 1) == sA.charAt(startA + jj - 1))
+                    SM[ii][jj] = SM[ii - 1][jj - 1] + 1;
+                else
+                    SM[ii][jj] = max(SM[ii][jj - 1], SM[ii - 1][jj]);
+            }
+
+        }
+
+        /*
+        #print 'Matrix (%d, %d)' %(i,j)
+                #printMatrix(SM)
+        ##if (i+1) == gH:
+                ##	print 'Somente direita'
+                ##  	return ([m[lsizeA] for m in SM],[])
+                ##else:
+         */
+        int saida[] = new int[lsizeA+1];
+        for(int o = 0; o < SM.length; o++){
+            saida[o] = SM[o][lsizeA];
+        }
+
+        Object[] retorno = new Object[2];
+        retorno[0] = SM[lsizeB];
+        retorno[1] = saida;
+        return retorno;
+    }
+
+    static int max(int number, int anotherNumber){
+        int max = number;
+        if(anotherNumber > number)
+            max = anotherNumber;
+        return max;
+    }
+
 }
