1076b920dd9bac9a7bd840a3506d886b49e884b9
mpcrocha <mpc_rocha@hotmail.com>
2017-01-21 21:50:10 -0200

    implements serializable

--- src/examples/vrc/Color.java
+++ src/examples/vrc/Color.java
@@ -1,9 +1,11 @@
 package examples.vrc;
 
+import java.io.Serializable;
+
 /**
  * Created by alexandrenery on 10/26/16.
  */
-public class Color {
+public class Color implements Serializable{
     public float r;
     public float g;
     public float b;
